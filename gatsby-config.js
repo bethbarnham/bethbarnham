637997cbd64201e53b35411c6ed58ed24808dd2d
Beth Barnham <37490855+b3thsb@users.noreply.github.com>
2022-07-15 10:52:42 +0100

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -80,8 +80,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Foundation`,
-        short_name: `Foundation`,
+        name: `Beth Barnham`,
+        short_name: `Beth Barnham`,
         start_url: `/`,
         background_color: `#f7f0eb`,
         theme_color: `#a2466c`,
